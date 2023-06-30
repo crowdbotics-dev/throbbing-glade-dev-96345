8fee6055d3bb8e1b92a23e1f8621d2b3aa297df7
Crowdbotics <engineering@crowdbotics.com>
2023-06-30 23:14:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "throbbing-glade-dev-96345",
+  url: "https://throbbing-glade-dev-96345.botics.co",
+  api: "https://throbbing-glade-dev-96345.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
